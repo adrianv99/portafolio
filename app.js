b4aa3d2b709b348b76393c24975353a541308d5b
Adrian Vinas <adrianvinasrosa@gmail.com>
2020-12-11 12:23:20 -0400

    add picture section and animations

--- app.js
+++ app.js
@@ -9,3 +9,72 @@ menu.addEventListener('click', () => {
     menuList.classList.toggle('active');
     body.classList.toggle('active');
 });
+
+// Animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-hero', {
+    duration: 1,
+    opacity: 0,
+    y: -175,
+    stagger: 0.7
+});
+
+gsap.from('.animate-about', {
+    scrollTrigger: '.animate-about',
+    duration: 1,
+    opacity: 0,
+    x: -175,
+    stagger: 0.3,
+});
+
+gsap.from('.animate-skills', {
+    scrollTrigger: '.animate-skills',
+    duration: 1,
+    opacity: 0,
+    y: 175,
+    stagger: 0.15,
+});
+
+gsap.from('.animate-project', {
+    scrollTrigger: '.animate-project',
+    duration: 1,
+    opacity: 0,
+    x: -175,
+    stagger: 0.3,
+});
+
+gsap.from('.animate-projects', {
+    scrollTrigger: '.animate-projects',
+    duration: 1,
+    opacity: 0,
+    y: 175,
+    stagger: 0.5,
+    delay: 0.5
+});
+
+gsap.from('.animate-picture', {
+    scrollTrigger: '.animate-picture',
+    duration: 1,
+    opacity: 0,
+    x: -175,
+    stagger: 0.3,
+});
+
+gsap.from('.animate-gallery-item', {
+    scrollTrigger: '.animate-gallery-item',
+    duration: 1.5,
+    opacity: 0,
+    y: 175,
+    stagger: 0.5,
+    delay: 0.5
+});
+
+gsap.from('.animate-socials', {
+    scrollTrigger: '.animate-socials',
+    duration: 1,
+    opacity: 0,
+    x: -175,
+    stagger: 0.3,
+    delay: 0.3
+});
